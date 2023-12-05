4807c832eac7bdd35c9f7c5a2447d364659be432
ramanti prajna <ramanti@ramantis-MacBook-Air.local>
2023-12-06 02:53:32 +0700

    record memory usage

--- generateDataset.py
+++ generateDataset.py
@@ -2,6 +2,8 @@ import random
 import time
 from DP import minSizeVertexCover
 from BNB import main as BnB_main
+import tracemalloc
+
 
 def generate_random_tree(num_vertices):
     cur_vertex = 1
@@ -16,19 +18,22 @@ def generate_random_tree(num_vertices):
 
     return adj_list
 
+
 def truncate_tree(num_vertices, adj_list):
     adj = adj_list.copy()
     M = num_vertices[0]
-    for i in range(1, M+1):
+    for i in range(1, M + 1):
         if i <= M:
             adj[i] = [elem for elem in adj[i] if elem <= M]
-    return adj[:M+1]
+    return adj[: M + 1]
+
 
 def save_tree_to_file(tree, filename):
-    with open(filename, 'w') as f:
+    with open(filename, "w") as f:
         f.write(f"{len(tree) - 1} {sum(len(neighbors) for neighbors in tree[1:])} 0\n")
         for i, neighbors in enumerate(tree[1:], start=1):
             f.write(f"{' '.join(map(str, neighbors))}\n")
+
 
 def generate_and_run_algorithms(num_vertices, dp_filename, bnb_filename, cutoff_time):
     dp_tree = generate_random_tree(num_vertices)
@@ -38,15 +43,22 @@ def generate_and_run_algorithms(num_vertices, dp_filename, bnb_filename, cutoff_
     save_tree_to_file(bnb_tree, bnb_filename)
 
     print(f"\nRunning DP Algorithm on {num_vertices[1]} vertices...")
+    tracemalloc.start()
     start_time = time.time()
     minSizeVertexCover(dp_tree, num_vertices[1])
     dp_execution_time = time.time() - start_time
+    _, dp_memory_usage = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
 
     print(f"\nRunning BnB Algorithm on {num_vertices[0]} vertices...")
+    tracemalloc.start()
     BnB_main(bnb_filename, "Output/", cutoff_time, None)
     bnb_execution_time = time.time() - start_time - dp_execution_time
+    _, bnb_memory_usage = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
 
-    return dp_execution_time, bnb_execution_time
+    return dp_execution_time, dp_memory_usage, bnb_execution_time, bnb_memory_usage
+
 
 def main():
     sizes = [
@@ -60,12 +72,17 @@ def main():
         dp_filename = f"tree_DP_{num_vertices[1]}.txt"
         bnb_filename = f"tree_BnB_{num_vertices[0]}.txt"
 
-        dp_time, bnb_time = generate_and_run_algorithms(num_vertices, dp_filename, bnb_filename, cutoff_time)
+        dp_time, dp_memory, bnb_time, bnb_memory = generate_and_run_algorithms(
+            num_vertices, dp_filename, bnb_filename, cutoff_time
+        )
 
         print(f"\nResults for {num_vertices[1]} vertices:")
         print(f"DP Algorithm Execution Time: {dp_time} seconds")
+        print(f"DP Algorithm Memory Usage: {dp_memory} bytes")
         print(f"BnB Algorithm Execution Time: {bnb_time} seconds")
+        print(f"BnB Algorithm Memory Usage: {bnb_memory} bytes")
         print("=" * 40)
+
 
 if __name__ == "__main__":
     main()
